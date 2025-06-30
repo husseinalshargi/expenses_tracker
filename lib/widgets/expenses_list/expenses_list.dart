66c8d1c7e9b7f2cbb57645f858dcb9dd6907a37d
Hussein Adel <x.sky.x@hotmail.com>
2025-06-30 20:10:08 +0300

    Add expense removal functionality with undo option and update UI handling

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -3,16 +3,29 @@ import 'package:expence_traker/widgets/expenses_list/expenses_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+    required this.onRemoveExpense,
+  });
+
+  final Function(Expense expense) onRemoveExpense;
 
   final List<Expense> expenses;
 
   @override
   Widget build(BuildContext context) {
     //builder only creates items when it is needed
-    return ListView.builder(//ctx and index is automatically provided, it will be called "itemCount" num of times
+    return ListView.builder(
+      //ctx and index is automatically provided, it will be called "itemCount" num of times
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpensesItem(expenses[index]),
-    ); 
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
+        child: ExpensesItem(expenses[index]),
+      ),
+    );
   }
 }

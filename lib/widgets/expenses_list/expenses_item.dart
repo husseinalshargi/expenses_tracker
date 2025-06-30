5e50c3fc4ee37f9c11a08844695fd7c5f2b0a7a4
Hussein Adel <x.sky.x@hotmail.com>
2025-06-30 21:54:22 +0300

    adding theme to appdata

--- lib/widgets/expenses_list/expenses_item.dart
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:expence_traker/models/expense.dart';
 import 'package:flutter/material.dart';
 
@@ -14,18 +12,23 @@ class ExpensesItem extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(expense.title),
+            Text(expense.title, style: Theme.of(context).textTheme.titleLarge),
             const SizedBox(height: 4),
             Row(
               children: [
-                Text("\$${expense.amount.toStringAsFixed(2)}"), //12.23434 -> 12.23
+                Text(
+                  "\$${expense.amount.toStringAsFixed(2)}",
+                ), //12.23434 -> 12.23
                 const Spacer(), //takes the space between both
-                Row(children: [
-                  Icon(categoryIcons[expense.category]),
-                  const SizedBox(width: 6,),
-                  Text(expense.FormattedDate)
-                ],),
+                Row(
+                  children: [
+                    Icon(categoryIcons[expense.category]),
+                    const SizedBox(width: 6),
+                    Text(expense.FormattedDate),
+                  ],
+                ),
               ],
             ),
           ],
